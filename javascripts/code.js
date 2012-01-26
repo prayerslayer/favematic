9dd8bd677976378cd34b57f305c8c4a6c5fd06ef
prayerslayer <prayerslayer@gmail.com>
2012-01-26 18:53:38 +0100

    tried to circumvent the not-firing events in firefox with $.scPlayer, but no luck. my attempts are in the comments.

--- javascripts/code.js
+++ javascripts/code.js
@@ -3,6 +3,7 @@
 //TODO statt jquery ajax calls vl SC.put, .get etc benutzen?
 //TODO IE unterstützung
 //TODO FF widget events?
+//TODO eingeloggt neu laden -> keine events von widgets
 
 var favorites = new Array();
 var max_tracks = 10;
@@ -21,41 +22,48 @@ shuffle favorites
 make soundcloud widgets of first 10
 */
 jQuery.support.cors = true;
+$.scPlayer.defaults.onDomReady = null;
 //returning from soundcloud
-var param_idx = window.location.href.indexOf('?');
-if (param_idx>0 &&  window.location.href.slice(window.location.href.indexOf('?')+1, window.location.href.indexOf('='))==="code") {
-	code = window.location.href.slice(window.location.href.indexOf('?')+1, window.location.href.length);
-	code = code.substring("code=".length);
-	console.log("post req");
-	$.ajax({
-		"url": "https://api.soundcloud.com/oauth2/token?client_id="+client_id+"&client_secret="+client_s+"&redirect_uri=http://localhost:8000/favematic&grant_type=authorization_code&code="+code,
-		"type": "POST",
-		error: function(a,b,c) {
-			console.log("error");
-			console.log(JSON.stringify(a));
-		},
-		success: function(a,b,c) {
-			if (typeof(a)==="string") {
-				a = jQuery.parseJSON(a);
+if (!SC.isConnected()) {
+	var param_idx = window.location.href.indexOf('?');
+	if (param_idx>0 &&  window.location.href.slice(window.location.href.indexOf('?')+1, window.location.href.indexOf('='))==="code") {
+		code = window.location.href.slice(window.location.href.indexOf('?')+1, window.location.href.length);
+		code = code.substring("code=".length);
+		console.log("post req");
+		$.ajax({
+			"url": "https://api.soundcloud.com/oauth2/token?client_id="+client_id+"&client_secret="+client_s+"&redirect_uri=http://localhost:8000/favematic&grant_type=authorization_code&code="+code,
+			"type": "POST",
+			"dataType": "JSON",
+			error: function(a,b,c) {
+				console.log("error");
+				console.log(JSON.stringify(a));
+			},
+			success: function(a,b,c) {
+				if (typeof(a)==="string") {
+					a = jQuery.parseJSON(a);
+				}
+				token = a.access_token;
+				SC.accessToken(token);
+				console.log(token);
+				SC.get("/me", function(me) {
+					curUserId = me.id;
+					$("#login_msg").hide();
+					$("#next").show();
+					$("#playlist").show();
+					$("#connect").hide();
+					$("#disconnect").show();
+					bindToSCEvents();
+					getUserFollowings(curUserId);
+				});
 			}
-			token = a.access_token;
-			SC.accessToken(token);
-			console.log(token);
-			SC.get("/me", function(me) {
-				curUserId = me.id;
-				$("#login_msg").hide();
-				$("#next").show();
-				$("#playlist").show();
-				$("#connect").hide();
-				$("#disconnect").show();
-				bindToSCEvents();
-				getUserFollowings(curUserId);
-			});
-		}
-	});
+		});
+	}
 }
 
 function bindToSCEvents() {
+	/*$(document).bind('onPlayerInit.scPlayer', function(event){
+	  console.log(event.target, 'is ready!');
+	});*/
 	$(document).bind("soundcloud:onPlayerError", function(player, data) {
 		console.log("player error", player, data);
 		var section = getSectionFromPlayer(player);
@@ -93,10 +101,18 @@ function bindToSCEvents() {
 }
 
 $(document).ready(function() {
-	$("#login").click(function() {
+	if (SC.isConnected()) {
+		//somehow somebody comes back connected
+		$("#connect").hide()
+		$("#disconnect").show();
+		getUserFollowings(curUserId);
+	}
+	$("#connect").click(function() {
 		if (!SC.isConnected())
-			window.location.href="https://soundcloud.com/connect?client_id=2becb25e8c48f8204593eb4df44c037f&response_type=code&redirect_uri=http://localhost:8000/favematic";
-		else {
+			window.location.replace("https://soundcloud.com/connect?client_id=2becb25e8c48f8204593eb4df44c037f&response_type=code&redirect_uri=http://localhost:8000/favematic");
+	});
+	$("#disconnect").click(function() {
+		if (SC.isConnected()) {
 			SC.disconnect();
 			$("#login_msg").show();
 			code=null;
@@ -108,6 +124,7 @@ $(document).ready(function() {
 			favorites = new Array();
 			$("#connect").show();
 			$("#disconnect").hide();
+			$("#next").hide();
 		}
 	});
 	$("#next").click(function() {
@@ -174,8 +191,11 @@ function createPlaylist(div) {
 	$("#"+div.attr("id")+" > .spinner").show();
 	var current_favs = favorites.slice(offset*max_tracks,Math.min((offset+1)*max_tracks, favorites.length)); //get current slice
 	$(current_favs).each(function (idx, fav) {	//add tracks to div
-		div.append(playerTemplate(fav, offset*max_tracks+idx));
+		var template = playerTemplate(fav, offset*max_tracks+idx);
+		console.log(template);
+		div.append(template);
 	});
+	$(".soundcloud-track").scPlayer();
 	$("#"+div.attr("id")+" > .spinner").hide();	//hide spinner
 }
 
@@ -202,10 +222,18 @@ function next() {
 	});
 	
 }
+/*
+function playerTemplate(track, idx) {
+	//var playerHTML = "<div class='soundcloud-track'><a href='"+track.permalink_url+"'>"+track.title+"</a></div>";
+	var spinner = "<img src='images/spinner.gif' class='spinner'/>";
+	var div =  "<div id='section"+idx+"' class='track'><p class='track_description'>"+track.title+"</p><p>faved by <a href='"+track.user.permalink_url+"'>"+track.fav_of+"</a></p><br/>"+spinner+" <div class='soundcloud-track'><a href='"+track.permalink_url+"'>ulu mulu</a></div></div>";
+	return div;
+}
+*/
 
 function playerTemplate(track, idx){
 	var playerHTML = '<object id="track'+idx+'" height="83.5em" width="100%">  <param name="movie" value="http://player.soundcloud.com/player.swf?object_id=track'+idx+'&enable_api=true&buying=false&color=000000&show_comments=false&show_user=true&url='+track.permalink_url+'"></param>  <param name="allowscriptaccess" value="always"></param>  <embed     src="http://player.soundcloud.com/player.swf?object_id=track'+idx+'&enable_api=true&buying=false&color=000000&show_comments=false&show_user=true&url='+track.permalink_url+'"&allowscriptaccess="always" height="83.5em" type="application/x-shockwave-flash" width="100%" name="track'+idx+'"></embed></object>';
 	var spinner = "<img src='images/spinner.gif' class='spinner'/>"
-	var div = "<div id='section"+idx+"' class='track'><p class='track_description'>"+track.title+"</p><p>faved by "+track.fav_of+"</p><br/>"+spinner+" "+playerHTML+"</div>";
+	var div = "<div id='section"+idx+"' class='track'><p class='track_description'>"+track.title+"</p><p>faved by <a href='"+track.user.permalink_url+"'>"+track.fav_of+"</a></p><br/>"+spinner+" "+playerHTML+"</div>";
 	return div;
 }

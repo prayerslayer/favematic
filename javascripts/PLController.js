b9eac4558cf168bf7e863f82d065cafff5ccf7a5
Nikolaus Piccolotto <prayerslayer@gmail.com>
2012-02-03 21:50:07 +0100

    new branch for mvc restructuring

---
+++ javascripts/PLController.js
@@ -0,0 +1,6 @@
+var PLController = {
+
+	model: null,
+	view: null,
+	
+};
---
+++ javascripts/PLModel.js
@@ -0,0 +1,20 @@
+var PLModel = {
+	//settings
+	favorites: [],
+	clientID: "2becb25e8c48f8204593eb4df44c037f",
+	clientSecret: "c390106e460ae397444126c8c77a0213",
+
+	//methods
+	connect: function() {
+		
+	},
+	disconnect: function() {
+		
+	},
+	collectFavorites: function() {
+		
+	},
+	getFavorites: function(howmany) {
+		
+	}
+};
